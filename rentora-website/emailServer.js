adbca8b6c7b6bbe7912c1d880b74321d995e2ff9
vinayakvadoothker <74890320+vinayakvadoothker@users.noreply.github.com>
2024-02-14 15:18:12 -0800

    updates

--- rentora-website/emailServer.js
+++ rentora-website/emailServer.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const port = 3001;
+const port = 443;
 
 app.use(bodyParser.json());
 app.use(cors());
