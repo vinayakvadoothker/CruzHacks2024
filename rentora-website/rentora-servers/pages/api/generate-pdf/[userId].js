5a2033535bb8adb38113796b0f183337dd8aefad
vinayakvadoothker <74890320+vinayakvadoothker@users.noreply.github.com>
2024-02-24 02:14:26 -0800

    update servers

--- rentora-website/rentora-servers/pages/api/generate-pdf/[userId].js
+++ rentora-website/rentora-servers/pages/api/generate-pdf/[userId].js
@@ -548,8 +548,9 @@ export default async function handler(req, res) {
                 stream.end(pdfBytes);
             });
         }
-        const path = require('path');
-        const pdfTemplatePath = path.resolve(__dirname, './pages/api/generate-pdf/rentora-application.pdf');
+
+        const pdfTemplatePath = `${__dirname}/rentora-application.pdf`;
+
         // Then, use this function as before to generate and upload both versions of the PDF
         await createAndUploadPDF(pdfTemplatePath, `${userId}_filled.pdf`, userId, formData);
         await createAndUploadPDF(pdfTemplatePath, `${userId}_official_filled.pdf`, userId, formData);
